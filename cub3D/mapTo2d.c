f2451a8cacb7817c1ec4d66d2c11775afb0e9e07
mino0310 <bewriter310@gmail.com>
2021-05-05 14:18:05 +0900

    mlx study

--- cub3D/mapTo2d.c
+++ cub3D/mapTo2d.c
@@ -17,17 +17,17 @@
 
 typedef struct              s_img
 {
-    void                    *img_ptr;
+    void                    *img;
     int                     *data;
-    int                     size_l
+    int                     size_l;
     int                     bpp;
     int                     endian;
 }                           t_img;
 
 typedef struct              s_game
 {
-    void                    *mlx_ptr;
-    void                    *win_ptr;
+    void                    *mlx;
+    void                    *win;
     t_img                   img;
 
     int                     map[ROWS][COLS];
@@ -71,4 +71,106 @@ void                        draw_lines(t_game *game)
         draw_line(game, 0, j * TILE_SIZE, WIDTH, j * TILE_SIZE);
         j++;
     }
+    draw_line(game, 0, ROWS * TILE_SIZE - 1, WIDTH, ROWS * TILE_SIZE -1);
+}
+
+void                        draw_rectangle(t_game *game, int x, int y)
+{
+    int                     i;
+    int                     j;
+
+    x *= TILE_SIZE;
+    y *= TILE_SIZE;
+    i = 0;
+    while (i < TILE_SIZE)
+    {
+        j = 0;
+        while (j < TILE_SIZE)
+        {
+            game->img.data[(y + i) * WIDTH + x + j] = 0xFFFFFF;
+            j++;
+        }
+        i++;
+    }
+}
+
+void                        draw_rectangles(t_game *game)
+{
+    int                     i;
+    int                     j;
+
+    i = 0;
+    while (i < ROWS)
+    {
+        j = 0;
+        while (j < COLS)
+        {
+            if (game->map[i][j] == 1) draw_rectangle(game, j, i);
+            j++;
+        }
+        i++;
+    }
+}
+
+int                         deal_key(int key_code, t_game *game)
+{
+    if (key_code == KEY_ESC) exit(0);
+    return (0);
+}
+
+int                         close(t_game *game)
+{
+    exit(0);
+}
+
+void                        game_init(t_game *game)
+{
+    int                     map[ROWS][COLS] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	};
+    memcpy(game->map, map, sizeof(int) * ROWS * COLS);
+}
+
+void                        window_init(t_game *game)
+{
+    game->mlx = mlx_init();
+    game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "mlx 42");
+}
+
+void                        img_init(t_game *game)
+{
+    game->img.img = mlx_new_image(game->mlx, WIDTH, HEIGHT);
+    game->img.data = (int *)mlx_get_data_addr(game->img.img, &game->img.bpp, &game->img.size_l, &game->img.endian);
+}
+
+int                         main_loop(t_game *game)
+{
+    draw_rectangles(game);
+    draw_lines(game);
+    mlx_put_image_to_window(game->mlx, game->win, game->img.img, 0, 0);
+    return (0);
+}
+
+int                         main(void)
+{
+    t_game game;
+
+    game_init(&game);
+    window_init(&game);
+    img_init(&game);
+    mlx_hook(game.win, X_EVENT_KEY_PRESS, 0, &deal_key, &game);
+    mlx_hook(game.win, X_EVENT_KEY_EXIT, 0, &close, &game);
+
+    mlx_loop_hook(game.mlx, &main_loop, &game);
+    mlx_loop(game.mlx);
 }
